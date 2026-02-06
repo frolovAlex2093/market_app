a9d48be4016831d36192ce3b7668e815d7ca2365
frolov.alex <243@elpts.ru>
2026-02-06 09:25:10 +0300

    rewrite pom and models

--- src/main/java/ru/yandex/practicum/mymarket/model/Order.java
+++ src/main/java/ru/yandex/practicum/mymarket/model/Order.java
@@ -1,38 +1,18 @@
 package ru.yandex.practicum.mymarket.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-@Entity
-@Table(name = "orders")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table("orders")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
-    private LocalDateTime created = LocalDateTime.now();
-
-    @Column(nullable = false)
+    private LocalDateTime created;
     private Long totalSum;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "order_items",
-            joinColumns = @JoinColumn(name = "order_id"),
-            inverseJoinColumns = @JoinColumn(name = "item_id")
-    )
-    @Builder.Default
-    private List<OrderItem> items = new ArrayList<>();
 }
